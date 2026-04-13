dccdaf1dab454a2024e3147c283d4a996303cdc0
Luiz Lourenco <luiz.lourenco@outlook.com.br>
2026-04-13 00:36:15 -0400

    Primeiro commit

---
+++ src/main/java/br/dev/lourenco/scriba/ScribaApplication.java
@@ -0,0 +1,13 @@
+package br.dev.lourenco.scriba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScribaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScribaApplication.class, args);
+	}
+
+}
